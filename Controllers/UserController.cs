3c9d1023b68702253d415d085e202b8f97f60e65
Aman-bot-Coder <psofficial835@gmail.com>
2023-04-01 00:59:45 +0530

    Add project files.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+using UserRegistration.Models;
+
+namespace UserRegistration.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        public readonly IConfiguration _Configuration;
+        public UserController(IConfiguration configuration)
+        {
+            _Configuration = configuration;
+        }
+        [HttpPost]
+        [Route("/regis")]
+
+        public string model1(Model1 model1)
+        {
+            SqlConnection conn = new SqlConnection(_Configuration.GetConnectionString("TestOne").ToString());
+            SqlCommand cmd = new SqlCommand("INSERT INTO Registration(UserName ,Password,Email,isActive) VALUES('" + model1.UserName+ "','" +model1.Password+ "','" +model1.Email+ "','" +model1.isActive+ "')", conn);
+            conn.Open();
+            int i = cmd.ExecuteNonQuery();
+            conn.Close();
+            if (i > 0)
+            {
+                return "Data Inserted";
+            }
+            else
+            {
+                return "Error";
+            }
+            return "";
+        }
+
+
+    }
+}
---
+++ Models/Model1.cs
@@ -0,0 +1,11 @@
+﻿namespace UserRegistration.Models
+{
+    public class Model1
+    {
+        public int Id { get; set; }
+        public  String UserName { get; set; }
+        public String Password { get; set; }
+        public String Email { get; set; }
+        public int isActive { get; set; }
+    }
+}
